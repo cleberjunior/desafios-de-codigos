278837e55c92b392b42c64815bb34aca3a930210
Cleber Farias <jrf.bynho@hotmail.com>
2024-06-16 19:04:04 -0300

    add InverterNumeros

--- src/me/plataformas/adatech/InverterNumeros.java
+++ src/me/plataformas/adatech/InverterNumeros.java
@@ -1,2 +1,25 @@
-package me.plataformas.adatech.EstruturasDeDados;public class InverterNumeros {
+package me.plataformas.adatech;
+
+import me.plataformas.adatech.EstruturasDeDados.Stack;
+
+public class InverterNumeros {
+    public static void main(String[] args) {
+        int[] numeros = {5,4,3,2,1};
+
+        inverter(numeros);
+    }
+
+    private static void inverter(int[] numeros) {
+        Stack stack = new Stack(numeros[0]);
+
+        for (int i = 1; i < numeros.length; i++) {
+            stack.push(numeros[i]);
+        }
+
+        var node = stack.pop();
+        while (node != null) {
+            System.out.println(node.getValue());
+            node = stack.pop();
+        }
+    }
 }
